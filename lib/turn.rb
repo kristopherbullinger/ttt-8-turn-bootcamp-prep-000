fc62b75a1fa04b04a253a3c1ef984ba9437eedf6
Kristopher Bullinger <kris.bullinger@gmx.com>
2018-05-02 23:25:13 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -4,7 +4,7 @@ end
 
 def valid_move? (board, input)
   input -= 1 
-  if (input < 0 || input > board.length -1)
+  if (input < 0 || input > board.length-1)
     return "Invalid move"
   end
   if (board[input] == " " || board[input] == "" || board[input] == nil)
